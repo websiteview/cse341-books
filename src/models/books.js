22ef2b18dcd5714aae583ba6678a3daafe58dceb
byui-professional <men22027@byui.edu>
2026-09-12 14:43:43 -0600

    Add get book by id route

--- src/models/books.js
+++ src/models/books.js
@@ -4,4 +4,8 @@ const getAllBooks = async () => {
   return await getDb().collection('books').find({}).toArray();
 };
 
-export { getAllBooks };
+const getBookById = async (bookId) => {
+  return await getDb().collection('books').findOne({ id: bookId });
+};
+
+export { getAllBooks, getBookById };
--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getBooks } from './controllers/books.js';
+import { getBooks, getBook } from './controllers/books.js';
 
 const router = express.Router();
 
 router.get('/books', getBooks);
+router.get('/books/:id', getBook);
 
 export default router;
